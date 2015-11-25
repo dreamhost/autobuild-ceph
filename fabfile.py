5554ec546f1ad99fdfa1eccdfc98e63c825343ae
Robin H. Johnson <robbat2@gentoo.org>
2015-11-25 12:58:37 -0800

    fabfile: Support changing repo sources cleanly.
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- fabfile.py
+++ fabfile.py
@@ -395,6 +395,7 @@ def _deb_install_extras(ceph_build_uri='https://github.com/ceph/ceph-build.git',
         if not exists('ceph-build'):
             sudo('git clone -b {branch} {uri}'.format(uri=ceph_build_uri, branch=ceph_build_branch))
         with cd('ceph-build'):
+            sudo('git remote set-url origin {uri}'.format(uri=ceph_build_uri))
             sudo('git checkout origin/{branch}'.format(branch=ceph_build_branch))
             sudo('git pull')
         sudo('grep -q autobuild-ceph /etc/sudoers || echo "autobuild-ceph ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers')
